06c4aa968d7cd93d645d0f7679d5fc12b8ba58e3
lautiq <lautaroquarin@gmail.com>
2024-06-15 17:53:33 -0300

    Corregidos archivos de GPIO

--- inc/gpio.h
+++ inc/gpio.h
@@ -1,16 +1,65 @@
+#ifndef GPIO_H
+#define GPIO_H_
+
+/**
+ * @file gpio.h
+ * @author Lautaro Quarin (lautaroquarin@gmail.com)
+ * @brief API for controlling GPIOs
+ */
+/* === Header files inclusions ================================================================ */
 #include <stdint.h>
 #include <stdbool.h>
+/* === C++ Header ============================================================================ */
 
-// void gpioSetDirection(uint8_t puerto, uint8_t bit, bool salida);
-// void gpioSetOutput(uint8_t puerto, uint8_t bit, bool estado);
-// bool gpioGetInput(uint8_t puerto, uint8_t bit);
+#ifdef __cplusplus
+extern "C" {
+#endif
 
-typedef struct gpio_s * gpio_t;
+/* === Public macros definitions =============================================================== */
 
-gpio_t gpioCreate(uint8_t puerto, uint8_t bit);
+/* === Public data type declarations =========================================================== */
+typedef struct gpio_s * gpio_t; // pointer to anonymous structure
 
+/* === Public variable declarations ============================================================ */
+
+/* === Public function declarations ============================================================ */
+/**
+ * @brief GPIO instance constructor
+ *
+ * @param port 8-bit port to use
+ * @param bit GPIO pin to use
+ * @return gpio_t
+ */
+gpio_t gpioCreate(uint8_t port, uint8_t bit);
+
+/**
+ * @brief Configures the GPIO instance as input or output.
+ *
+ * @param gpio GPIO instance to work with
+ * @param output Desired state for the GPIO
+ */
 void gpioSetOutput(gpio_t gpio, bool output);
 
+/**
+ * @brief Sets the state of a pin configured as output.
+ *
+ * @param gpio GPIO instance to work with
+ * @param state Desired state for the pin
+ */
 void gpioSetState(gpio_t gpio, bool state);
 
+/**
+ * @brief Reads the state of an instance configured as input
+ *
+ * @param gpio GPIO instance to work with
+ * @return true - a high state is read
+ * @return false - a low state is read
+ */
 bool gpioGetState(gpio_t gpio);
+
+/* === End of documentation ==================================================================== */
+#ifdef __cplusplus
+}
+#endif
+
+#endif
--- src/gpio.c
+++ src/gpio.c
@@ -1,21 +1,33 @@
+/**
+ * @file: gpio.c
+ * @brief Source code for gpio.c
+ *
+ */
+
+/* === Headers files inclusions =============================================================== */
 #include "gpio.h"
+#include "inc\gpio.h"
 #include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
 
-#define USE_DYNAMIC_MEM
-
+/* === Public macros definitions =============================================================== */
 #ifndef GPIO_MAX_INSTANCES
 #define GPIO_MAX_INSTANCES 10
 #endif
 
+/* === Private data type definition ========================================================== */
 struct gpio_s {
     uint8_t port;
     uint8_t bit;
     bool output;
+    bool state;
     bool used;
 };
 
+/* === Private function definition ============================================================ */
 /**
- * @brief
+ * @brief Private function to create an instance without using dynamic memory
  *
  * @return gpio_t
  */
@@ -25,7 +37,7 @@ static gpio_t allocateInstance() {
     gpio_t result = NULL;
     for (int index = 0; index < GPIO_MAX_INSTANCES; index++) {
         if (!instances[index].used) {
-            result = &instances[index].used;
+            result = &instances[index];
             result->used = true;
             break;
         }
@@ -33,28 +45,33 @@ static gpio_t allocateInstance() {
     return result;
 }
 
-gpio_t gpioCreate(uint8_t puerto, uint8_t bit) {
+/* === Public function definition ============================================================ */
+gpio_t gpioCreate(uint8_t port, uint8_t bit) {
 #ifdef USE_DYNAMIC_MEM
     gpio_t self = malloc(sizeof(struct gpio_s));
 #else
     gpio_t self = allocateInstance();
 #endif
     if (self) {
-        self->port = puerto;
+        self->port = port;
         self->bit = bit;
         self->output = false;
     }
     return self;
 }
 
-void gpioSetOutput(gpio_t self, bool output) {
-    HAL_GPIO_SET_OUTPUT(self->port, self->bit);
+void gpioSetOutput(gpio_t gpio, bool output) {
+    gpio->output = output;
+    HAL_GPIO_SET_OUTPUT(gpio->port, gpio->bit);
 }
 
-void gpioSetState(gpio_t self, bool state) {
-    HAL_GPIO_SET_STATE(self->port, self->bit);
+void gpioSetState(gpio_t gpio, bool state) {
+    if (gpio->output) {
+        HAL_GPIO_SET_STATE(gpio->port, gpio->bit, state);
+        gpio->state = state; // Save the state internally
+    }
 }
 
-bool gpioGetState(gpio_t self) {
-    return HAL_GPIO_GET_STATE(self->port, self->bit);
+bool gpioGetState(gpio_t gpio) {
+    return HAL_GPIO_GET_STATE(gpio->port, gpio->bit);
 }
